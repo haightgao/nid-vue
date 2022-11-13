588b03f2841aee0cf8fea3dca22fb91fbd970fa9
haight <gao_ht@126.com>
2022-11-14 01:08:05 +0800

    定义应用图标组件

--- src/app/components/app-home.vue
+++ src/app/components/app-home.vue
@@ -1,12 +1,14 @@
 <template>
   <div>欢迎访问宁皓网：）</div>
-  <app-logo size="64" color="red" />
+  <AppLogo size="64" color="#6435c9" />
+  <AppIcon size="64" color="#6435c9" name="comment" />
 </template>
 
 <script>
 import AppLogo from '@/app/components/app-logo.vue';
+import AppIcon from '@/app/components/app-icon.vue';
 
 export default {
-  components: { AppLogo },
+  components: { AppLogo, AppIcon },
 };
 </script>
