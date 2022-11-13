6cec86005dced6826011c95bc8e49a24d2171f44
haight <gao_ht@126.com>
2022-11-14 01:00:57 +0800

    定义应用标志组件

--- src/app/components/app-home.vue
+++ src/app/components/app-home.vue
@@ -1,3 +1,12 @@
 <template>
   <div>欢迎访问宁皓网：）</div>
+  <app-logo size="64" color="red" />
 </template>
+
+<script>
+import AppLogo from '@/app/components/app-logo.vue';
+
+export default {
+  components: { AppLogo },
+};
+</script>
