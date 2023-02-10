c6f5669212b602e92ccbb3af0d1db09bdfaf3f7f
haight <gao_ht@126.com>
2023-02-10 20:14:46 +0800

    fix: 清理 AppHome 组件

---
+++ src/app/layout/components/page-header.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="page-header">
+    <div class="container">
+      <AppToolbar />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import AppToolbar from '@/app/toolbar/app-toolbar.vue';
+
+export default defineComponent({
+  name: 'PageHeader',
+
+  components: { AppToolbar },
+});
+</script>
+
+<style scoped></style>
